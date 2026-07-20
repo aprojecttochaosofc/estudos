eae768f60110fb4bf6a802d23e01b6426cc5f3d4
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-20 16:49:54 -0300

    Update updateplayer.js

--- usersconfig/updateplayer.js
+++ usersconfig/updateplayer.js
@@ -8,8 +8,28 @@ const pool = new Pool({
 
 module.exports = function updateplayer(ws, data, players) {
 
-    ws.send(JSON.stringify({
-        message: "snapshot",
-        user: data.user
-    }));
+    const player = players[data.userid];
+
+    if (!player) {
+        return;
+    }
+
+    player.x = data.x;
+    player.y = data.y;
+    player.z = data.z;
+
+    player.rx = data.rx;
+    player.ry = data.ry;
+    player.rz = data.rz;
+
+    player.armtrx = data.armtrx;
+    player.armtry = data.armtry;
+    player.armtrz = data.armtrz;
+
+    player.walk = data.walk;
+    player.run = data.run;
+    player.onground = data.onground;
+    player.atack = data.atack;
+
+    console.log(players);
 };
