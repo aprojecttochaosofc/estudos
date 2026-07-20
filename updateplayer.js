0f65587d97b93086bae8731a121b100c265ca10e
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-20 16:31:08 -0300

    Update updateplayer.js

--- updateplayer.js
+++ updateplayer.js
@@ -1,7 +1,15 @@
+const { Pool } = require("pg");
+const callconfigs = require("./config");
 
+const pool = new Pool({
+    connectionString: callconfigs("postgre"),
+    ssl: { rejectUnauthorized: false }
+});
 
+module.exports = function updateplayer(ws, data, players) {
 
-module.exports = function updateplayer(req, res) {
- 
-
+    ws.send(JSON.stringify({
+        message: "snapshot",
+        user: data.user
+    }));
 };
