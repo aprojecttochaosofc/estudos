37e6828a490f8bbaf50d91e71efd291cf14aab64
aprojecttochaosofc <aprojecttochaosofc@gmail.com>
2026-07-20 17:00:16 -0300

    Update disconnectuser.js

--- usersconfig/disconnectuser.js
+++ usersconfig/disconnectuser.js
@@ -1,1 +1,14 @@
 
+module.exports = function disconnectuser(ws, data, players) {
+
+    const user = data.user;
+
+    if (players[user]) {
+
+        delete players[user];
+
+        console.log("Usuário removido:", user);
+
+    }
+
+};
